5ca9ee74665dae525b4bf4a3c476b2a04cc0362d
NhuVo47 <vongochuynhnhu040703@gmail.com>
2025-11-21 00:26:38 +0700

    commit truoc khi pull

--- backend/user_service/src/main/java/com/healthcare/user_service/config/SecurityConfig.java
+++ backend/user_service/src/main/java/com/healthcare/user_service/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/api/reports/**").permitAll()
                         .requestMatchers("/api/patients/**").hasAnyAuthority("PATIENT", "DOCTOR", "ADMIN")
                         .anyRequest().authenticated()
                 )
